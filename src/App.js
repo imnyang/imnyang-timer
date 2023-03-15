44977dd9da032bd15dd45fc65c997e74dae8e263
imnyang <nyang.nyanglab@gmail.com>
2023-03-15 13:15:49 +0900

    bug pan

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './App.css'
-function Timer({ minutes = 3, seconds = 0}) {
+function Timer() {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [timeLeft, setTimeLeft] = useState(minutes * 60 + seconds);
